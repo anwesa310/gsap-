5a5f8055b185855905ea92cc167c8a876a115820
Anwesa Ghosh <anwesa310@gmail.com>
2025-11-01 18:02:37 +0530

    Text Animations

---
+++ vid6/script.js
@@ -0,0 +1,31 @@
+function breakText(){
+    var h1=document.querySelector("h1")
+    var h1Text=document.querySelector("h1").textContent
+    var splittedText= h1Text.split("")
+
+    var halfValue=Math.floor(splittedText.length/2)
+    var clutter=""
+    splittedText.forEach(function(elem,idx){
+        if(idx<halfValue){
+             clutter+= `<span class="a">${elem}</span>`
+        }else{
+            clutter+= `<span class="b">${elem}</span>`
+        }
+})
+h1.innerHTML=clutter
+}
+breakText()
+gsap.from("h1 .a",{
+    y:50,
+    opacity:0,
+    duration:0.6,
+    delay:0.5,
+    stagger:0.15
+})
+gsap.from("h1 .b",{
+    y:50,
+    opacity:0,
+    duration:0.6,
+    delay:0.5,
+    stagger:-0.15
+})
